f42834d6c9daec95c1cd54386f856ecdadc376db
Alexandre Cadeau <cadeaua.stud@gmail.com>
2017-04-12 13:57:03 +0200

    add app

---
+++ front/app/route.js
@@ -0,0 +1,37 @@
+function Router(){
+  let routes = [];
+
+  function checkIfItsRoutes(event){
+    let location = window.location.pathname;
+    
+    let currentRoutes = routes.find(route => route.path == location);
+
+    if(currentRoutes !== undefined){
+      document.getElementById('app').innerHTML = currentRoutes.components.template
+      currentRoutes.components.initialiseEvents();
+    } else {
+      document.getElementById('app').innerHTML = "Error"
+    }
+  }
+  window.onpopstate = checkIfItsRoutes;
+
+  return {
+    path: function(path, components){
+      routes.push({path, components})
+    },
+    start: () => checkIfItsRoutes(),
+    go: (path, state) => {
+      window.history.pushState(state, path, path);
+      checkIfItsRoutes();
+    }
+  }
+}
+
+let router = new Router();
+
+router.path("/", new Home());
+
+router.start();
+
+
+
